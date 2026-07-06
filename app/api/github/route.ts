95e4955e1ecf3d9b02794f6145417de40f69ea94
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-06 21:24:54 +0800

    Set dynamic export for GitHub API route

--- app/api/github/route.ts
+++ app/api/github/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { Octokit } from "@octokit/rest";
-
+export const dynamic = 'force-dynamic'
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const owner = url.searchParams.get("owner");
