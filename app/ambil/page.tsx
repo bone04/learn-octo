a14324225858a8a5d3305e87d8d973ed4f6e5cdf
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-07 23:03:15 +0800

    Remove GitHub content fetching logic from AmbilPage
    Removed Octokit integration and related environment variables for GitHub content fetching.

--- app/ambil/page.tsx
+++ app/ambil/page.tsx
@@ -1,27 +1,9 @@
-import { Octokit } from "@octokit/rest";
 // https://raw.githubusercontent.com/bone04/learn-octo/main/items.json
 
 export default async function AmbilPage() {
-
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN || undefined
-  });
+    // const ambilData = fetch();
   
- const owner: string = process.env.GITHUB_OWNER || "bone04"
-  const repo: string = process.env.GITHUB_REPO || "learn-octo"
-  const path: string = process.env.GITHUB_FILE_PATH || "items.json"
-
-  const response = await octokit.rest.repos.getContent({
-      owner,
-      repo,
-      path
-    });
-    if (!data) {
-    return {
-      notFound: true,
-    }
-  }
-  return (
+    return (
     <div>
       <h1>Hello Next.js! Ambil Page</h1>
       <p>This is a basic page in Next.js.</p>
