beaef5cc6fe5cf968b184f10319f9c5210b6b08b
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-07 23:02:18 +0800

    Add GET route to fetch GitHub content
    Implement GET route to fetch content from GitHub repository

---
+++ app/api/ambil/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { Octokit } from "@octokit/rest";
+export const dynamic = 'force-static';
+// https://raw.githubusercontent.com/bone04/learn-octo/main/items.json
+
+export async function GET(request: Request) {
+  const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN || undefined
+    });
+    
+   const owner: string = process.env.GITHUB_OWNER || "bone04"
+  const repo: string = process.env.GITHUB_REPO || "learn-octo"
+  const path: string = process.env.GITHUB_FILE_PATH || "items.json"
+
+  const response = await octokit.rest.repos.getContent({
+      owner,
+      repo,
+      path
+    });
+
+}
