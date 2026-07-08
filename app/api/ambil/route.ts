6a3e799a6fd6b23968112756c3d5e2d199ad7ea0
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-08 20:20:37 +0800

    Update route.ts

--- app/api/ambil/route.ts
+++ app/api/ambil/route.ts
@@ -4,18 +4,20 @@ export const dynamic = 'force-static';
 // https://raw.githubusercontent.com/bone04/learn-octo/main/items.json
 
 export async function GET(request: Request) {
+  
+  const owner: string = process.env.GITHUB_OWNER || "bone04"
+  const repo: string = process.env.GITHUB_REPO || "learn-octo"
+  const path: string = process.env.GITHUB_FILE_PATH || "items.json"
+
   const octokit = new Octokit({
       auth: process.env.GITHUB_TOKEN || undefined
     });
     
-   const owner: string = process.env.GITHUB_OWNER || "bone04"
-  const repo: string = process.env.GITHUB_REPO || "learn-octo"
-  const path: string = process.env.GITHUB_FILE_PATH || "items.json"
-
   const response = await octokit.rest.repos.getContent({
       owner,
       repo,
       path
     });
-
+  
+return NextResponse.json("response route");
 }
