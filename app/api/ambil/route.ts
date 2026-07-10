2b53f8e628d8e688f593cef15165a4b026707845
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-10 23:33:18 +0800

    Integrate Octokit to retrieve GitHub repo content
    Added Octokit integration to fetch repository content based on environment variables.

--- app/api/ambil/route.ts
+++ app/api/ambil/route.ts
@@ -11,5 +11,31 @@ export async function GET() {
  if (!owner) {
       throw new Error("OWNER environment variable is required.");
     }
+  const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN || undefined
+    });
+    
+  const  { data } = await octokit.rest.repos.getContent({
+      owner,
+      repo,
+      path
+    });
+  
+  if (!Array.isArray(data)) return [];
+  
+  /*
+  if (!Array.isArray(data)) { // Filter out directories
+
+    if (data.type === 'file') { // Make sure it's a file
+      data.content;
+    }
+  }
+  */
+  /*
+    if (!Array.isArray(data) && data.content) {
+      const content = Buffer.from(data.content, "base64").toString("utf8");
+      console.log(content);
+      }
+  */
 return NextResponse.json({ message: "Data received" });
 }
